3c87203a8c49e4ca381a530612fd10821448eed9
Robert Anthony Bellamy <rabellamy@gmail.com>
2015-05-04 13:21:50 -0400

    adds correct map coordinates

--- js/DevOps-map.js
+++ js/DevOps-map.js
@@ -1,12 +1,27 @@
 
 var map;
+
 function initialize() {
+
+  'use strict';
+
+  var DevOpsCampLatlng = new google.maps.LatLng(40.75011, -73.96874);
+
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 16,
+    center: DevOpsCampLatlng,
+    mapMaker: true
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: DevOpsCampLatlng,
+    title: 'The United Nations'
+  });
+
+  marker.setMap(map);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
