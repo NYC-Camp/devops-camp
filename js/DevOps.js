411c05f9143ddb55815157fe803b7aa934d704ac
Robert Anthony Bellamy <rabellamy@gmail.com>
2015-05-05 14:07:59 -0400

    adds back to top icon

--- js/DevOps.js
+++ js/DevOps.js
@@ -27,6 +27,28 @@
       $('nav ul').toggleClass('nav-active');
     });
 
+    // back to top
+    (function($) {
+
+      var offset = 250,
+          duration = 860;
+
+      $(window).scroll(function() {
+          if ($(this).scrollTop() > offset) {
+              $('#back-to-top').fadeIn(duration);
+          } else {
+              $('#back-to-top').fadeOut(duration);
+          }
+      });
+
+      $('#back-to-top').click(function(event) {
+          event.preventDefault();
+          $('html, body').animate({scrollTop: 0}, duration);
+          return false;
+      })
+
+    })($);
+
   });
 
 
